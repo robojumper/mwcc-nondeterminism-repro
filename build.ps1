7f508194d162b7594bdc6390aaf13e8a25c0662e
robojumper <robojumper@gmail.com>
2026-05-01 10:39:33 +0200

    Fewer flags

--- build.ps1
+++ build.ps1
@@ -1,12 +1,6 @@
 $ccargs = @(
   "-nodefaults",
-  "-proc", "gekko",
-  "-align", "powerpc",
-  "-fp", "hardware",
-  "-Cpp_exceptions", "off",
-  "-O2,p",
-  "-nosyspath",
-  "-RTTI", "off"
+  "-O2,p"
 )
 
 & "../ss/build/tools/sjiswrap.exe" "../ss/build/compilers/Wii/1.5/mwcceppc.exe" $ccargs -c code.c
